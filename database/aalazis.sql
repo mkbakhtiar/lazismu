31936e1d9ee23fcbe2469b61d886fbb70b12b323
Muhammad Khoirul Bakhtiar <mkbakhtiar.new@gmail.com>
2023-05-19 10:01:18 +0700

    add migration table muzakys, bank, bank_type, budget, programs, transaction

--- database/aalazis.sql
+++ database/aalazis.sql
@@ -1,19 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : localhost
- Source Server Type    : MySQL
- Source Server Version : 50724
- Source Host           : localhost:3306
- Source Schema         : aalazis
-
- Target Server Type    : MySQL
- Target Server Version : 50724
- File Encoding         : 65001
-
- Date: 19/05/2023 09:33:08
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
